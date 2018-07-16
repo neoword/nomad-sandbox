976e6bcd41af296f62f4fb35e2b020661d46bee0
Rene X. Parra <rparra@homeaway.com>
2018-07-16 13:47:24 -0500

    Removed unnecessary files
    - These files have been moved to confluent-sandbox project

--- scripts/start_zk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Execue this script by doing the following:
-# vagrant ssh node2 -c /vagrant/scripts/start_zk.sh
-NOMAD_ADDR="http://192.168.33.11:4646" nomad job run /vagrant/jobs/zookeeper.hcl
